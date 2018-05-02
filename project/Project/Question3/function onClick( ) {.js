ef4697453e41692f2dfc6c078072354e53fe8d04
hperez9 <31747764+hperez9@users.noreply.github.com>
2018-05-02 14:23:53 -0400

    Add files via upload

---
+++ project/Project/Question3/function onClick( ) {.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("picture").innerHTML =
+    "<img src =  'http://2.bp.blogspot.com/-lJlg4TsmNyo/UF5EmNF_DjI/AAAAAAAABms/OUrcsoGHSpM/s1600/small-meal.jpg' />";
+}
+
+function myFunction1() {
+  document.getElementById("picture").innerHTML =
+    "<img src =  'http://imagenes.montevideo.com.uy/imgnoticias/201504/499669.jpg' />";
+}
